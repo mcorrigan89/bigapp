678fb40beb22ae7adcfa7d9b81defcd751300961
ebreitsch <evanbreitsch@users.noreply.github.com>
2025-02-01 13:40:21 -0600

    revent some file changes

--- client/src/icons/components/Tent.tsx
+++ client/src/icons/components/Tent.tsx
@@ -1,7 +1,7 @@
 import { Icon } from "../lib/Icon";
 import { IconNode, SVGIconProps } from "../lib/types";
 
-const _iconNode: IconNode = [["path",{"d":"M3.5 21 14 3M20.5 21 10 3M15.5 21 12 15l-3.5 6M2 21h20","key":"m6lj3jdx"}]];
+const _iconNode: IconNode = [["path",{"d":"M3.5 21 14 3M20.5 21 10 3M15.5 21 12 15l-3.5 6M2 21h20","key":"m68w66c8"}]];
 
 export const Tent = (props: SVGIconProps) => (
   <Icon iconNode={_iconNode} {...props} />
